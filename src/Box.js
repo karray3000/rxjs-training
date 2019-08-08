b32d9a737d621519703ad1184b98e7b259eb9205
karray3000 <karray3000@gmail.com>
2019-08-08 17:10:19 +0200

    full code

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,38 @@
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { fromEvent } from 'rxjs';
+import { skipUntil, takeUntil, repeat, distinctUntilChanged, mergeMap } from 'rxjs/operators';
 
+// drag and drop component
 const Box = () => {
   const [xCoordinate, setXCoordinate] = useState(0);
   const [yCoordinate, setYCoordinate] = useState(0);
   const ref = useRef()
+
+  useEffect(() => {
+    const move$ = fromEvent(document, 'mousemove');
+    const up$ = fromEvent(document, 'mouseup');
+    const down$ = fromEvent(ref.current, 'mousedown');
+    const criterion = (e) => `${Math.floor(e.x / 10)}-${Math.floor(e.y / 10)}`;
+
+    // First method
+    // const drag$ = move$.pipe(
+    //   distinctUntilChanged((a, b) => criterion(a) === criterion(b)),
+    //   skipUntil(down$),
+    //   takeUntil(up$),
+    //   repeat()
+    // )
+  
+    // 
+    const drag$ = down$.pipe(
+      mergeMap((down) => move$.pipe(takeUntil(up$)))
+    )
+
+    drag$.subscribe((e) => {
+      console.log('drag');
+      setXCoordinate(e.x);
+      setYCoordinate(e.y)
+    })
+  }, []);
   
   return (
   <div
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { ajax } from 'rxjs/ajax';
+import { fromEvent, timer } from 'rxjs';
+import { flatMap, throttle } from 'rxjs/operators';
 
 function Button() {
   const ref = useRef();
@@ -7,6 +9,16 @@ function Button() {
     const userId = Math.round(Math.random() * 10);
     return ajax(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
   };
+  useEffect(() => {
+    const click$ = fromEvent(ref.current, 'mousedown')
+      .pipe(
+        throttle(() => timer(300)),
+        flatMap(getAlbums)
+      );
+    click$.subscribe((value) => { console.log(value); })
+  }, []);
+
+  
   return (<button ref={ref}>lol</button>);
 }
 
